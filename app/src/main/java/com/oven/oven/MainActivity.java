a60de43e69142099f8e6b1e85f036d53158dd559
benjamin0326 <benjamin0326@naver.com>
2017-07-08 14:17:41 +0900

    tmp

--- app/src/main/java/com/oven/oven/MainActivity.java
+++ app/src/main/java/com/oven/oven/MainActivity.java
@@ -1,13 +1,31 @@
 package com.oven.oven;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        Button.OnClickListener btn_listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ItemListActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        btn_login.setOnClickListener(btn_listener);
     }
 }
