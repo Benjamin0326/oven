9051aa8f4bef43084a110818a3dd908f9ca1923c
benjamin0326 <benjamin0326@naver.com>
2017-07-29 12:35:48 +0900

    kakao login tmp

--- app/src/main/java/com/oven/oven/MainActivity.java
+++ app/src/main/java/com/oven/oven/MainActivity.java
@@ -1,16 +1,25 @@
 package com.oven.oven;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 import com.oven.oven.layout.ItemListActivity;
+import com.oven.oven.layout.KakaoSignupActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_login;
+    private SessionCallback callback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +37,48 @@ public class MainActivity extends AppCompatActivity {
         };
 
         btn_login.setOnClickListener(btn_listener);
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            redirectSignupActivity();  // 세션 연결성공 시 redirectSignupActivity() 호출
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+            setContentView(R.layout.activity_main); // 세션 연결이 실패했을때
+        }                                            // 로그인화면을 다시 불러옴
+    }
+
+    protected void redirectSignupActivity() {       //세션 연결 성공 시 SignupActivity로 넘김
+        final Intent intent = new Intent(this, KakaoSignupActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
---
+++ app/src/main/java/com/oven/oven/layout/KakaoSignupActivity.java
@@ -0,0 +1,74 @@
+package com.oven.oven.layout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.helper.log.Logger;
+import com.oven.oven.MainActivity;
+import com.oven.oven.R;
+
+public class KakaoSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestMe();
+    }
+
+    protected void requestMe() { //유저의 정보를 받아오는 함수
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String message = "failed to get user info. msg=" + errorResult;
+                Logger.d(message);
+
+                ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                    finish();
+                } else {
+                    redirectLoginActivity();
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.d("KakaoError", "SessionClosed");
+                redirectLoginActivity();
+            }
+
+            @Override
+            public void onNotSignedUp() {} // 카카오톡 회원이 아닐 시 showSignup(); 호출해야함
+
+            @Override
+            public void onSuccess(UserProfile userProfile) {  //성공 시 userProfile 형태로 반환
+                Logger.d("UserProfile : " + userProfile);
+                String kakaoID = String.valueOf(userProfile.getId()); // userProfile에서 ID값을 가져옴
+                String kakaoNickname = userProfile.getNickname();     // Nickname 값을 가져옴
+                Log.d("UserInfo :", kakaoID+" / "+kakaoNickname);
+                redirectMainActivity(); // 로그인 성공시 MainActivity로
+            }
+        });
+    }
+
+    private void redirectMainActivity() {
+        startActivity(new Intent(this, ItemListActivity.class));
+        finish();
+    }
+    protected void redirectLoginActivity() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
