817424cdff4f2da57ed72cce43aba45a7d0c301b
benjamin0326 <benjamin0326@naver.com>
2017-09-20 19:19:03 +0900

    제품 상세보기 수정

--- app/src/main/java/com/oven/oven/layout/ItemDetailActivity.java
+++ app/src/main/java/com/oven/oven/layout/ItemDetailActivity.java
@@ -101,20 +101,20 @@ public class ItemDetailActivity extends AppCompatActivity {
                     if(response.body()!=null){
                         productDetail = response.body();
                         if(productDetail.getCode()==200){
-                            Picasso.with(ItemDetailActivity.this).load(productDetail.getProduct_detail().get(0).getImage()).resize(500,500).centerCrop().into(img_item_detail);
-                            tv_item_name.setText(productDetail.getProduct_detail().get(0).getPname());
+                            Picasso.with(ItemDetailActivity.this).load(productDetail.getImage()).resize(500,500).centerCrop().into(img_item_detail);
+                            tv_item_name.setText(productDetail.getPname());
                             String testText = "";
-                            testText+=("가격 : "+productDetail.getProduct_detail().get(0).getPrice());
-                            testText+=("\n최소주문 수량 : "+productDetail.getProduct_detail().get(0).getMin());
-                            testText+=("\n배송예정일 : "+productDetail.getProduct_detail().get(0).getArrival_date());
-                            testText+=("\n유통기한 : "+productDetail.getProduct_detail().get(0).getExpire_date());
-                            testText+=("\n보관방법 : "+productDetail.getProduct_detail().get(0).getStorage());
-                            testText+=("\n주의사항 : "+productDetail.getProduct_detail().get(0).getNotice());
-                            testText+=("\n상세설명 : "+productDetail.getProduct_detail().get(0).getDescription());
+                            testText+=("가격 : "+productDetail.getPrice());
+                            testText+=("\n최소주문 수량 : "+productDetail.getMin());
+                            testText+=("\n배송예정일 : "+productDetail.getArrival_date());
+                            //testText+=("\n유통기한 : "+productDetail.getProduct_detail().get(0).getExpire_date());
+                            //testText+=("\n보관방법 : "+productDetail.getProduct_detail().get(0).getStorage());
+                            testText+=("\n주의사항 : "+productDetail.getNotice());
+                            testText+=("\n상세설명 : "+productDetail.getDescription());
                             tv_item_des.setText(testText);
-                            if(productDetail.getProduct_detail().get(0).getToggle()==1)
+                            if(productDetail.getToggle()==1)
                                 btn_like.setImageResource(R.mipmap.icon_item_action_like_selected);
-                            else if(productDetail.getProduct_detail().get(0).getToggle()==0)
+                            else if(productDetail.getToggle()==0)
                                 btn_like.setImageResource(R.mipmap.icon_item_action_like_deselected);
                         }
                     }
--- app/src/main/java/com/oven/oven/model/ProductDetailList.java
+++ app/src/main/java/com/oven/oven/model/ProductDetailList.java
@@ -7,9 +7,17 @@ import java.util.List;
  */
 
 public class ProductDetailList {
-    private int code;
-    private String msg;
+    private int code, min, max, toggle;
+    private String msg, pname, image, price, arrival_date, notice, description;
+    /*
     private List<ProductDetail> product_detail;
+    public void setProduct_detail(List<ProductDetail> _product_detail){
+        product_detail = _product_detail;
+    }
+    public List<ProductDetail> getProduct_detail(){
+        return product_detail;
+    }
+     */
 
     public void setCode(int _code){
         code = _code;
@@ -17,16 +25,66 @@ public class ProductDetailList {
     public void setMsg(String _msg){
         msg = _msg;
     }
-    public void setProduct_detail(List<ProductDetail> _product_detail){
-        product_detail = _product_detail;
+    public void setMin(int _min){
+        min = _min;
     }
+    public void setMax(int _max){
+        max = _max;
+    }
+    public void setToggle(int _toggle){
+        toggle = _toggle;
+    }
+    public void setPname(String _pname){
+        pname = _pname;
+    }
+    public void setImage(String _image){
+        image = _image;
+    }
+    public void setPrice(String _price){
+        price = _price;
+    }
+    public void setArrival_date(String _arrival_date){
+        arrival_date = _arrival_date;
+    }
+    public void setNotice(String _notice){
+        notice = _notice;
+    }
+    public void setDescription(String _description){
+        description = _description;
+    }
+
     public int getCode(){
         return code;
     }
     public String getMsg(){
         return msg;
     }
-    public List<ProductDetail> getProduct_detail(){
-        return product_detail;
+    public int getMin(){
+        return min;
     }
+    public int getMax(){
+        return max;
+    }
+    public int getToggle(){
+        return toggle;
+    }
+    public String getPname(){
+        return pname;
+    }
+    public String getImage(){
+        return image;
+    }
+    public String getPrice(){
+        return price;
+    }
+    public String getArrival_date(){
+        return arrival_date;
+    }
+    public String getNotice(){
+        return notice;
+    }
+    public String getDescription(){
+        return description;
+    }
+
 }
